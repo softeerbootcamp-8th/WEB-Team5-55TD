40f727580deb22bc95ae9b64c1cdf6246a09f7f6
Jihyeong Hong <topo.or.dubu@gmail.com>
2026-07-20 17:04:24 +0900

    feat: Swaager 설정 및 헬스체크 API 명세 추가

--- pickup/src/main/java/com/ootd/pickup/healthCheck/controller/HealthCheckController.java
+++ pickup/src/main/java/com/ootd/pickup/healthCheck/controller/HealthCheckController.java
@@ -1,5 +1,6 @@
 package com.ootd.pickup.healthCheck.controller;
 
+import com.ootd.pickup.healthCheck.api.HealthCheckApi;
 import com.ootd.pickup.healthCheck.controller.dto.response.HealthCheckResponse;
 import com.ootd.pickup.healthCheck.service.HealthCheckService;
 import lombok.RequiredArgsConstructor;
@@ -11,11 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/healthcheck")
 @RequiredArgsConstructor
-public class HealthCheckController {
+public class HealthCheckController implements HealthCheckApi {
 
     private final HealthCheckService healthCheckService;
 
     @GetMapping
+    @Override
     public ResponseEntity<HealthCheckResponse> healthCheck(){
         return ResponseEntity.ok(healthCheckService.getHealthCheckStatus());
     }
