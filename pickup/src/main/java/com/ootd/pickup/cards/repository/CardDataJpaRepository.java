85f0147bb2b1619b201973208ee1dd6b930001aa
Jihyeong Hong <topo.or.dubu@gmail.com>
2026-07-24 12:01:18 +0900

    feat: 카드 검색 쿼리 파라미터를 통해 카드 객체 조회

--- pickup/src/main/java/com/ootd/pickup/cards/repository/CardDataJpaRepository.java
+++ pickup/src/main/java/com/ootd/pickup/cards/repository/CardDataJpaRepository.java
@@ -1,10 +1,17 @@
 package com.ootd.pickup.cards.repository;
 
+import static com.ootd.pickup.cards.domain.QCard.card;
+
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import com.ootd.pickup.cards.domain.Card;
+import com.ootd.pickup.cards.domain.Language;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,10 +19,63 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class CardDataJpaRepository implements CardRepository {
     private final CardJpaRepository cardJpaRepository;
+    private final JPAQueryFactory queryFactory;
 
     @Override
     public Optional<Card> findCardById(Long cardId) {
         return cardJpaRepository.findCardByCardId(cardId);
     }
 
+    @Override
+    public List<Card> searchCards(
+            String keyword,
+            String setName,
+            Language language,
+            Long cursor,
+            int size
+    ) {
+        return queryFactory
+                .selectFrom(card)
+                .where(
+                        cardNameContains(keyword),
+                        setNameEq(setName),
+                        languageEq(language),
+                        cardIdLt(cursor)
+                )
+                .orderBy(card.cardId.desc())
+                .limit(size)
+                .fetch();
+    }
+
+    private BooleanExpression cardNameContains(String keyword) {
+        if (!StringUtils.hasText(keyword)) {
+            return null;
+        }
+
+        return card.cardName.contains(keyword);
+    }
+
+    private BooleanExpression setNameEq(String setName) {
+        if (!StringUtils.hasText(setName)) {
+            return null;
+        }
+
+        return card.setName.eq(setName);
+    }
+
+    private BooleanExpression languageEq(Language language) {
+        if (language == null) {
+            return null;
+        }
+
+        return card.language.eq(language);
+    }
+
+    private BooleanExpression cardIdLt(Long cursor) {
+        if (cursor == null) {
+            return null;
+        }
+
+        return card.cardId.lt(cursor);
+    }
 }
