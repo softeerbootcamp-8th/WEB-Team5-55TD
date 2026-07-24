74339c1e2b413b24d428a09bb234e219fb47e913
Jihyeong Hong <topo.or.dubu@gmail.com>
2026-07-24 11:38:50 +0900

    feat: 카드 검색 controller 메서드 작성

--- pickup/src/main/java/com/ootd/pickup/cards/controller/CardController.java
+++ pickup/src/main/java/com/ootd/pickup/cards/controller/CardController.java
@@ -4,11 +4,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ootd.pickup.cards.api.CardApi;
 import com.ootd.pickup.cards.dto.response.GetCardDetailResponse;
+import com.ootd.pickup.cards.dto.response.SearchCardsResponse;
 import com.ootd.pickup.cards.service.CardService;
+import com.ootd.pickup.global.dto.response.CursorPageResponse;
 
 import lombok.RequiredArgsConstructor;
 
@@ -25,4 +28,14 @@ public class CardController implements CardApi {
         return ResponseEntity.ok(cardService.getCardDetail(cardId));
     }
 
+    @GetMapping
+    public ResponseEntity<CursorPageResponse<SearchCardsResponse>> searchCards(
+            @RequestParam(required = false) String keyword,
+            @RequestParam(required = false) String setName,
+            @RequestParam(required = false) String language,
+            @RequestParam(required = false) String cursor,
+            @RequestParam Integer size
+    ) {
+        return ResponseEntity.ok(cardService.searchCards(keyword, setName, language, cursor, size));
+    }
 }
--- pickup/src/main/java/com/ootd/pickup/cards/service/CardService.java
+++ pickup/src/main/java/com/ootd/pickup/cards/service/CardService.java
@@ -2,12 +2,16 @@ package com.ootd.pickup.cards.service;
 
 import static com.ootd.pickup.global.exception.ExceptionCode.*;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ootd.pickup.cards.domain.Card;
 import com.ootd.pickup.cards.dto.response.GetCardDetailResponse;
+import com.ootd.pickup.cards.dto.response.SearchCardsResponse;
 import com.ootd.pickup.cards.repository.CardRepository;
+import com.ootd.pickup.global.dto.response.CursorPageResponse;
 import com.ootd.pickup.global.exception.PickUpException;
 
 import lombok.RequiredArgsConstructor;
@@ -25,4 +29,13 @@ public class CardService {
         return GetCardDetailResponse.from(card);
     }
 
+    public CursorPageResponse<SearchCardsResponse> searchCards(
+            String keyword,
+            String setName,
+            String language,
+            String cursor,
+            Integer size
+    ) {
+        return CursorPageResponse.from(List.of(), false, null);
+    }
 }
