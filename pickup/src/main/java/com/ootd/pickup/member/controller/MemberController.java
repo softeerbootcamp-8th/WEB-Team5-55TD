61dab525412c86438f0083b229c8d8ae23977853
Juhye0k <kyoung1678@naver.com>
2026-07-26 00:13:00 +0900

    feat : 회원가입 컨트롤러 Swagger 구현

--- pickup/src/main/java/com/ootd/pickup/member/controller/MemberController.java
+++ pickup/src/main/java/com/ootd/pickup/member/controller/MemberController.java
@@ -1,12 +1,13 @@
 package com.ootd.pickup.member.controller;
 
+import com.ootd.pickup.member.api.MemberApi;
 import com.ootd.pickup.member.dto.MemberRequest;
 import com.ootd.pickup.member.dto.MemberResponse;
 import com.ootd.pickup.member.service.MemberService;
 import jakarta.validation.Valid;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.HttpStatus;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,11 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/members")
 @RequiredArgsConstructor
-public class MemberController {
+public class MemberController implements MemberApi {
 
     private final MemberService memberService;
 
     @PostMapping
+    @Override
     public ResponseEntity<MemberResponse> createMember(@Valid @RequestBody MemberRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(memberService.createMember(request));
