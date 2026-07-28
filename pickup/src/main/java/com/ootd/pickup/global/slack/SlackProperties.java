e86c2e41e2a9c4762caa4cb27f30f913d39c823d
gibeom <delphox60@gmail.com>
2026-07-28 11:07:50 +0900

    feat: slack 에러 발송 구현

---
+++ pickup/src/main/java/com/ootd/pickup/global/slack/SlackProperties.java
@@ -0,0 +1,11 @@
+package com.ootd.pickup.global.slack;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+
+@ConfigurationProperties(prefix = "slack")
+public record SlackProperties(
+        @DefaultValue("false") boolean enabled,
+        String webhookUrl
+) {
+}
