99a881d27bd397776641b016513470071f13506e
gibeom <delphox60@gmail.com>
2026-08-02 10:31:43 +0900

    [OOTD-239] feat: 경매 입찰 내역 조회 API 구현
    GET /auctions/{auctionId}/bids 커서 페이지네이션 API를 추가한다.
    닉네임은 마스킹해서 노출하고, 비로그인 상태에서도 조회 가능하며
    로그인한 회원 본인의 입찰에는 isMine=true를 표시한다.

--- pickup/src/main/java/com/ootd/pickup/bid/repository/BidDataJpaRepository.java
+++ pickup/src/main/java/com/ootd/pickup/bid/repository/BidDataJpaRepository.java
@@ -1,7 +1,13 @@
 package com.ootd.pickup.bid.repository;
 
+import static com.ootd.pickup.bid.domain.QBid.bid;
+import static com.ootd.pickup.member.domain.QMember.member;
+
 import com.ootd.pickup.bid.domain.Bid;
 import com.ootd.pickup.bid.domain.BidStatus;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -11,6 +17,7 @@ import org.springframework.stereotype.Repository;
 public class BidDataJpaRepository implements BidRepository {
 
   private final BidJpaRepository bidJpaRepository;
+  private final JPAQueryFactory queryFactory;
 
   @Override
   public Bid save(Bid bid) {
@@ -21,5 +28,21 @@ public class BidDataJpaRepository implements BidRepository {
   public Optional<Bid> findFirstByAuctionIdAndBidStatus(Long auctionId, BidStatus bidStatus) {
     return bidJpaRepository.findFirstByAuctionAuctionIdAndBidStatusOrderByBidPriceDesc(
         auctionId, bidStatus);
+  }
+
+  @Override
+  public List<Bid> findAllByAuctionId(Long auctionId, Long cursorBidId, int limit) {
+    return queryFactory
+        .selectFrom(bid)
+        .join(bid.member, member)
+        .fetchJoin()
+        .where(bid.auction.auctionId.eq(auctionId), cursorPredicate(cursorBidId))
+        .orderBy(bid.bidId.desc())
+        .limit(limit)
+        .fetch();
+  }
+
+  private BooleanExpression cursorPredicate(Long cursorBidId) {
+    return cursorBidId == null ? null : bid.bidId.lt(cursorBidId);
   }
 }
--- pickup/src/main/java/com/ootd/pickup/bid/repository/BidRepository.java
+++ pickup/src/main/java/com/ootd/pickup/bid/repository/BidRepository.java
@@ -2,6 +2,7 @@ package com.ootd.pickup.bid.repository;
 
 import com.ootd.pickup.bid.domain.Bid;
 import com.ootd.pickup.bid.domain.BidStatus;
+import java.util.List;
 import java.util.Optional;
 
 public interface BidRepository {
@@ -9,4 +10,6 @@ public interface BidRepository {
   Bid save(Bid bid);
 
   Optional<Bid> findFirstByAuctionIdAndBidStatus(Long auctionId, BidStatus bidStatus);
+
+  List<Bid> findAllByAuctionId(Long auctionId, Long cursorBidId, int limit);
 }
