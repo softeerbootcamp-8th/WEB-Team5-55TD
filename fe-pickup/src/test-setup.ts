072a2d89ec8c4a1ff9502fdf6b77031016eaffad
delphox60 <delphox60@gmail.com>
2026-08-08 14:41:36 +0900

    [OOTD-365/FE] test: 도메인 컴포넌트 테스트 확장

--- fe-pickup/src/test-setup.ts
+++ fe-pickup/src/test-setup.ts
@@ -1,1 +1,10 @@
 import "@testing-library/jest-dom/vitest";
+
+Object.defineProperty(URL, "createObjectURL", {
+  writable: true,
+  value: (value: Blob) => `blob:test-${value.size}`,
+});
+Object.defineProperty(URL, "revokeObjectURL", {
+  writable: true,
+  value: () => undefined,
+});
