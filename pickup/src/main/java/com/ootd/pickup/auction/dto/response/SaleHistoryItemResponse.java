8fcd84b6f36c7610b6221b339865bea7109d3d7b
delphox60 <delphox60@gmail.com>
2026-08-13 15:53:21 +0900

    fix: 판매 내역에 상품 이미지 제공

--- pickup/src/main/java/com/ootd/pickup/auction/dto/response/SaleHistoryItemResponse.java
+++ pickup/src/main/java/com/ootd/pickup/auction/dto/response/SaleHistoryItemResponse.java
@@ -8,14 +8,17 @@ import com.ootd.pickup.consignments.domain.Certificate;
 public record SaleHistoryItemResponse(
     Long auctionId,
     GetCardDetailResponse card,
+    String thumbnailUrl,
     String grade,
     Long winningPrice,
     AuctionStatus resultType) {
 
-  public static SaleHistoryItemResponse of(Auction auction, Certificate certificate) {
+  public static SaleHistoryItemResponse of(
+      Auction auction, Certificate certificate, String thumbnailUrl) {
     return new SaleHistoryItemResponse(
         auction.getAuctionId(),
         GetCardDetailResponse.from(auction.getConsignment().getCard()),
+        thumbnailUrl,
         certificate != null ? certificate.getGradeDisplay() : null,
         auction.getWinningPrice(),
         auction.getAuctionStatus());
