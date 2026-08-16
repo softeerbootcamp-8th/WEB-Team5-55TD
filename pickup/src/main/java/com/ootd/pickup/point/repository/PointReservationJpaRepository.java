97dcde70186041974067ec060d79c546efc6aea2
Jihyeong Hong <topo.or.dubu@gmail.com>
2026-08-16 22:24:16 +0900

    [OOTD-544/BE] refactor: 포인트 예약 락 조회 QueryDSL 적용

--- pickup/src/main/java/com/ootd/pickup/point/repository/PointReservationJpaRepository.java
+++ pickup/src/main/java/com/ootd/pickup/point/repository/PointReservationJpaRepository.java
@@ -1,21 +1,6 @@
 package com.ootd.pickup.point.repository;
 
 import com.ootd.pickup.point.domain.PointReservation;
-import jakarta.persistence.LockModeType;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-public interface PointReservationJpaRepository extends JpaRepository<PointReservation, Long> {
-
-  @Query(
-      "select reservation.pointReservationId from PointReservation reservation where reservation.auction.auctionId = :auctionId")
-  Optional<Long> findIdByAuctionId(@Param("auctionId") Long auctionId);
-
-  @Lock(LockModeType.PESSIMISTIC_WRITE)
-  @Query(
-      "select reservation from PointReservation reservation where reservation.pointReservationId = :id")
-  Optional<PointReservation> findByIdForUpdate(@Param("id") Long id);
-}
+public interface PointReservationJpaRepository extends JpaRepository<PointReservation, Long> {}
--- pickup/src/test/java/com/ootd/pickup/point/repository/PointReservationDataJpaRepositoryTest.java
+++ pickup/src/test/java/com/ootd/pickup/point/repository/PointReservationDataJpaRepositoryTest.java
@@ -1,12 +1,15 @@
 package com.ootd.pickup.point.repository;
 
+import static com.ootd.pickup.point.domain.QPointReservation.pointReservation;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.never;
 
 import com.ootd.pickup.point.domain.PointReservation;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.LockModeType;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,19 +21,26 @@ import org.mockito.junit.jupiter.MockitoExtension;
 class PointReservationDataJpaRepositoryTest {
 
   @Mock private PointReservationJpaRepository pointReservationJpaRepository;
+  @Mock private JPAQueryFactory queryFactory;
+  @Mock private JPAQuery<Long> reservationIdQuery;
+  @Mock private JPAQuery<PointReservation> reservationQuery;
 
   private PointReservationDataJpaRepository pointReservationRepository;
 
   @BeforeEach
   void setUp() {
     pointReservationRepository =
-        new PointReservationDataJpaRepository(pointReservationJpaRepository);
+        new PointReservationDataJpaRepository(pointReservationJpaRepository, queryFactory);
   }
 
   @Test
   void 예약이_없으면_예약식별자만_조회하고_쓰기락을_획득하지_않는다() {
     // given
-    given(pointReservationJpaRepository.findIdByAuctionId(1L)).willReturn(Optional.empty());
+    given(queryFactory.select(pointReservation.pointReservationId)).willReturn(reservationIdQuery);
+    given(reservationIdQuery.from(pointReservation)).willReturn(reservationIdQuery);
+    given(reservationIdQuery.where(pointReservation.auction.auctionId.eq(1L)))
+        .willReturn(reservationIdQuery);
+    given(reservationIdQuery.fetchOne()).willReturn(null);
 
     // when
     Optional<PointReservation> reservation =
@@ -38,22 +48,30 @@ class PointReservationDataJpaRepositoryTest {
 
     // then
     assertThat(reservation).isEmpty();
-    then(pointReservationJpaRepository).should(never()).findByIdForUpdate(anyLong());
+    then(queryFactory).should(never()).selectFrom(pointReservation);
   }
 
   @Test
   void 예약이_있으면_식별자를_조회한_뒤_해당예약에_쓰기락을_획득한다() {
     // given
     PointReservation reservation = org.mockito.Mockito.mock(PointReservation.class);
-    given(pointReservationJpaRepository.findIdByAuctionId(1L)).willReturn(Optional.of(10L));
-    given(pointReservationJpaRepository.findByIdForUpdate(10L))
-        .willReturn(Optional.of(reservation));
+    given(queryFactory.select(pointReservation.pointReservationId)).willReturn(reservationIdQuery);
+    given(reservationIdQuery.from(pointReservation)).willReturn(reservationIdQuery);
+    given(reservationIdQuery.where(pointReservation.auction.auctionId.eq(1L)))
+        .willReturn(reservationIdQuery);
+    given(reservationIdQuery.fetchOne()).willReturn(10L);
+    given(queryFactory.selectFrom(pointReservation)).willReturn(reservationQuery);
+    given(reservationQuery.where(pointReservation.pointReservationId.eq(10L)))
+        .willReturn(reservationQuery);
+    given(reservationQuery.setLockMode(LockModeType.PESSIMISTIC_WRITE))
+        .willReturn(reservationQuery);
+    given(reservationQuery.fetchOne()).willReturn(reservation);
 
     // when
     Optional<PointReservation> found = pointReservationRepository.findByAuctionIdForUpdate(1L);
 
     // then
     assertThat(found).containsSame(reservation);
-    then(pointReservationJpaRepository).should().findByIdForUpdate(10L);
+    then(reservationQuery).should().setLockMode(LockModeType.PESSIMISTIC_WRITE);
   }
 }
